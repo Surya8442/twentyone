ce368b3009022c8a601cb7df0792f3c393d69f1c
Surya8442 <suryakandipalli1@gmail.com>
2026-03-23 00:31:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// Serve index.html from root
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(3000, () => console.log('Server running'));
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
