d671f94a1c9c7c55faf0e354073e2345ba217eca
Surya8442 <suryakandipalli1@gmail.com>
2026-03-23 00:23:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,9 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const path = require('path');
+const app = express();
 
-const server = http.createServer((req, res) => {
-  fs.readFile('index.html', (err, data) => {
-    if (err) {
-      res.writeHead(500);
-      res.end("Error loading page");
-    } else {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end(data);
-    }
-  });
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-server.listen(3000, '0.0.0.0', () => {
-  console.log("Server running on port 3000");
-});
+app.listen(3000, () => console.log('Server running'));
